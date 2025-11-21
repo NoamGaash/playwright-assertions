597a87bd315f101493e61ba7194361a0a00acaa7
Noam Gaash <noam.gaash@applitools.com>
2025-11-21 20:28:43 +0200

    initial commit

---
+++ playwright.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+	testDir: "./tests",
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: 0,
+	workers: process.env.CI ? 1 : undefined,
+	reporter: "html",
+	projects: [
+		{
+			use: { ...devices["Desktop Chrome"] },
+		},
+	],
+});
